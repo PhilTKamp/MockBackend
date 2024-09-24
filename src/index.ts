a175b3b5a0cfe31738f769bbf9d9ae6644fa3106
Philip Telkamp <ptelkamp@microsoft.com>
2024-09-24 16:04:29 -0700

    Updating express to allow requests from any domain

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ import {
 const app = express();
 const port = 3000;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
